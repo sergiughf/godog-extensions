e82997b8145527f783abecdfdfe5d4be613c1a87
Sergiu Ghitea <sg@hellofresh.com>
2020-08-06 12:41:11 +0200

    Close db after each scenario in db extension

--- database.go
+++ database.go
@@ -25,14 +25,24 @@ func NewPostgresCleanup(ctx *godog.ScenarioContext, postgresDSN string) {
         $func$ LANGUAGE plpgsql;
     `
 
-	db, err := sql.Open("postgres", postgresDSN)
-	if err != nil {
-		log.Fatalf("failed to connect to postgres while executing the db clean up: %+v", err.Error())
-	}
-
+	var db *sql.DB
+	var err error
 	ctx.BeforeScenario(func(scenario *godog.Scenario) {
+		db, err = sql.Open("postgres", postgresDSN)
+		if err != nil {
+			log.Fatalf("failed to connect to postgres while executing the db clean up: %+v", err.Error())
+		}
+
 		if _, err := db.Exec(cleanupQuery); err != nil {
 			log.Fatalf("failed to execute db cleanup before scenarios: %+v", err.Error())
+		}
+	})
+
+	ctx.AfterScenario(func(scenario *godog.Scenario, err error) {
+		if db != nil {
+			if err := db.Close(); err != nil {
+				log.Fatalf("failed to execute db cleanup after scenarios: %+v", err.Error())
+			}
 		}
 	})
 }
